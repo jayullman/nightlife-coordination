13862a72c95a298fdb22d9e40b892f259b7c21a3
Jay Ullman <jayullman@gmail.com>
2017-05-07 20:22:13 -0700

    Add twitter auth callback for production environment

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,9 @@
+const cbURL = process.env.cbURL || 'http://127.0.0.1:3000/auth/twitter/callback'
+
 module.exports = {
   twitterAuth: {
     consumerKey: 'IuX2kh4D44j4PLeR1QcRZqmw1',
     consumerSecret: 'OAiUtcFLcdED1MAdD48SgsvBlagmzivBYzOGkeE3dD8HzkwCjI',
-    callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback'
+    callbackURL: cbURL
   }
 }
